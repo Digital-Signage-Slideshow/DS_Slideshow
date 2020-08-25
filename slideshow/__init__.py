c7a9eca961fa5e266d5f739f3de86357ff523351
Harry <52263746+Harry-L-01@users.noreply.github.com>
2020-08-25 10:54:38 +0100

    added support for starting server in docker

--- slideshow/__init__.py
+++ slideshow/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 
 from .extensions import db, migrate, bcrypt, login_manager
-# from slideshow.user.models import User
 
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object('config.DevelopmentConfig')
-
+ 
     from .core.views import bp as core_bp
     from .user.views import bp as user_bp
 
@@ -16,8 +15,6 @@ def create_app():
     app.register_blueprint(user_bp)
 
     extensions(app)
-
-    # authentication(app, User)
 
     with app.app_context():
         db.create_all()
@@ -30,13 +27,3 @@ def extensions(app):
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-
-
-# def authentication(app, user_model):
-#     login_manager.login_view = 'user.login'
-#
-#     @login_manager.user_loader
-#     def load_user(user_id: str) -> User:
-#         # should always return 1 until other users are supported
-#         return User.query.get(int(user_id))
-
