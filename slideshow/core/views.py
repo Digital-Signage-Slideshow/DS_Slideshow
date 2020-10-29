f396c20dc9cb89e24c6959d16a2ce2312c51d953
Harry Lees <harry.lees@gmail.com>
2020-10-29 01:21:45 +0000

    finished transition to Bootstrap

--- slideshow/core/views.py
+++ slideshow/core/views.py
@@ -22,23 +22,19 @@ def custom_500(error: dict):
     response = jsonify({'message': error})
 
 
-@bp.route('/remove_image', methods=['GET', 'POST'])
-def remove_image():
+@bp.route('/remove_content', methods=['GET', 'POST'])
+def remove_content():
     if request.method == 'POST':
-        filename = request.form.get('image_id')
+        content_id = request.form.get('content_id')
+        content = Content.query.filter_by(path = content_id)
+        cont_object = content.first()
 
-        os.remove(f'{upload_folder}/{filename}')
-        Content.query.filter_by(type='file', path=filename).delete()
+        if cont_object.type == 'file':
+            os.remove(f'{upload_folder}/{cont_object.path}')
+            content.delete()
+        else:
+            content.delete()
         db.session.commit()
-
-    return redirect(url_for('core.setup'))
-
-
-@bp.route('/remove_link', methods=['GET', 'POST'])
-def remove_link():
-    link = request.form.get('link')
-    Content.query.filter_by(type='link', path=link).delete()
-    db.session.commit()
 
     return redirect(url_for('core.setup'))
 
@@ -67,7 +63,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(upload_folder, filename))
 
-            new_file = Content(type='file', path=filename)
+            new_file = Content(type = 'file', path = filename)
             db.session.add(new_file)
             db.session.commit()
     else:
@@ -89,6 +85,11 @@ def upload_link():
     return redirect(url_for('core.setup'))
 
 
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+
 @bp.route('/setup')
 @login_required
 def setup():
@@ -96,26 +97,22 @@ def setup():
 
     contents = Content.query.all()
 
-    # images = os.listdir(upload_folder)
-    # links = db.session.query(Content).filter(Content.type == 'link')
-
     return render_template(
-        'core/setup.html',
+        'setup.html',
         contents = contents,
-        # images=images,
-        # links=links,
         rotation_speed=rotation_speed // 1000
     )
 
 
-@bp.route('/')
+@bp.route('/slideshow')
 def slideshow():
+    print('called')
     images = os.listdir(upload_folder)
     links = db.session.query(Content).filter(Content.type == 'link')
 
     return render_template(
-        'core/index.html',
-        images=images,
-        links=[link.path for link in links],
-        rotation_speed=rotation_speed,
+        'slideshow.html',
+        images = images,
+        links = [link.path for link in links],
+        rotation_speed = rotation_speed,
     )
--- slideshow/user/views.py
+++ slideshow/user/views.py
@@ -13,35 +13,6 @@ def custom_500(error: dict):
     response = jsonify({'message': error})
 
 
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-
-    if current_user.is_authenticated:
-        return redirect(url_for('core.setup'))
-
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(
-            form.password.data).decode('utf-8')
-
-        user = User(
-            username='Admin',
-            password=hashed_password
-        )  # may allow multiple users in future
-        
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for('user.login'))
-
-    elif form.errors.get('password') == ['active_password']:
-        flash(
-            'there is already an active admin password set, please <a href = "/login">login</a> instead'
-        )
-
-    return render_template('user/register.html', form=form)
-
-
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -64,4 +35,4 @@ def login():
 @bp.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
-    return redirect(url_for('.login'))
+    return redirect(url_for('core.index'))
