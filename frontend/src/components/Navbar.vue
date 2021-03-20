d8eb95a0770a05a7bc5e32e4f9b264880e0688f6
Harry-Lees <harry.lees@gmail.com>
2021-03-20 11:42:28 +0000

    split frontend into separate folder

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+        <router-link to='/'>DS Slideshow</router-link>
+
+        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarContent'
+                aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+            <span class='navbar-toggler-icon'></span>
+        </button>
+
+        <div class='collapse navbar-collapse' id='navbarContent'>
+            <ul class='navbar-nav ml-auto'>
+                <li class='nav-item'>
+                    <a class='nav-link'>
+                        <i class='fas fa-cogs'></i>&ensp;Settings
+                    </a>
+                </li>
+
+                <div>
+                    <li class='nav-item'>
+                        <a class='nav-link'>
+                            <i class='fas fa-user-cog'></i>&nbsp;Profile
+                        </a>
+                    </li>
+
+                    <li class='nav-item'>
+                        <a class='nav-link'>
+                            <i class='fas fa-sign-out-alt'></i>Sign&nbsp;Out
+                        </a>
+                    </li>
+                </div>
+
+                <li class='nav-item'>
+                    <a class='nav-link' href='#'><i class='fab fa-'></i>Documentation</a>
+                </li>
+
+                <li class='nav-item'>
+                    <a
+                    class='nav-link fs-30 p-0' 
+                    href='https://github.com/Digital-Signage-Slideshow/DS_Slideshow'
+                    target='_blank'>
+                        <i class='fab fa-github'></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+        props: {
+            currentUser: Object
+        },
+    }
+</script>
---
+++ frontend/src/components/SlideDeck.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class='album pt-5'>
+        <div class='container'>
+            <button type='button' class='btn btn-primary ml-3'>Upload Content</button>
+            <a href='/slideshow' class='btn btn-outline-secondary ml-3'>View Slideshow</a>
+
+            <div class='album pt-4'>
+                <div class='container'>
+                    <h2>Content</h2>
+                    <p class='text-muted'>Drag and drop slides to reorder the slideshow (not implemented yet)</p>
+                    <hr>
+
+                    <div class='card-deck'>
+                        <SlideCard
+                        v-for='slide in slides'
+                        v-bind:slide='slide'
+                        :key='slide.title'
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SlideCard from '@/components/SlideCard.vue';
+
+    export default {
+        name: 'SlideDeck',
+
+        data() {
+            return {
+                rotation_speed: 10,
+            }
+        },
+
+        props: {
+            slides: Object,
+        },
+
+        components: {
+            SlideCard,
+        },
+    }
+</script>
