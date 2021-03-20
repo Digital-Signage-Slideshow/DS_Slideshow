0d781e9f13120c7f45451affea73c73c5d69d8ca
Harry-Lees <harry.lees@gmail.com>
2021-03-20 16:11:09 +0000

    removed redundant comment

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from slideshow import create_app
+from api import create_app
 
 app = create_app()
 
@@ -6,5 +6,4 @@ __author__ = 'Harry Lees'
 __date__ = '08.08.2020'
 
 if __name__ == '__main__':
-    # Remove host='0.0.0.0' on production environment
     app.run(host='0.0.0.0')
