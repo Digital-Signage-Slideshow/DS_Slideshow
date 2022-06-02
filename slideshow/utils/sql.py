e1d62276b05d5478d1d945a2a0a8b0a3ccb136e5
Javier Baez <baezdevs@gmail.com>
2022-06-01 22:58:22 -0500

    merged and removed classes

--- slideshow/utils/sql.py
+++ slideshow/utils/sql.py
@@ -3,43 +3,12 @@ from datetime import datetime
 from slideshow.extensions import db
 
 
-class TimestampMixin:
-    """
-    Adds timestamps to models (created & updated)
-    """
-    created_on = db.Column(db.DateTime, default=datetime.utcnow())
-    updated_on = db.Column(db.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
-
-
 class SQLMixin:
     """
-    Adds the ability to save and delete row's from database by passing a .save() or .delete()
+    Adds the ability to save and delete row's from database by passing a .save() .update() or .delete()
+    example: user = User(username='test').save() or user.update(username='test') or user.delete()
     """
 
-    def save(self):
-        """
-        Save the model instance
-        return: model instance
-        """
-        db.session.add(self)
-        db.session.commit()
-
-        return self
-
-    def delete(self):
-        """
-        Delete the model instance
-        return: commit session result
-        """
-        db.session.delete(self)
-        return db.session.commit()
-
-
-class DSMixin:
-    """
-    Digital Signage Slider custom mixin
-    adds timestamps to models & the ability to save and delete row's from database by passing a .save() or .delete()
-    """
     # adds created_on and updated_on to model
     created_on = db.Column(db.DateTime, default=datetime.utcnow())
     updated_on = db.Column(db.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
@@ -61,6 +30,9 @@ class DSMixin:
         """
         for key, value in kwargs.items():
             setattr(self, key, value)
+        db.session.commit()
+
+        return self
 
     def delete(self):
         """
