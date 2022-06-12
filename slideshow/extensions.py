7f2c8dace2e5c7603a16485540d7f2f087d83698
Javier Baez <baezdevs@gmail.com>
2022-06-12 00:42:58 -0500

    added IdModel class to SQLAlchemy db

--- slideshow/extensions.py
+++ slideshow/extensions.py
@@ -1,10 +1,29 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import Model, SQLAlchemy
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import sessionmaker
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
-db = SQLAlchemy()
+
+class IdModel(Model):
+    """
+    Base model class that includes an auto-incrementing primary key.
+    """
+
+    @declared_attr
+    def id(cls):
+        for base in cls.__mro__[1:-1]:
+            if getattr(base, 'id', None) is not None:
+                col_type = sa.ForeignKey(base.id, ondelete='CASCADE')
+                break
+        else:
+            col_type = sa.Integer()
+        return sa.Column(col_type, primary_key=True)
+
+
+db = SQLAlchemy(model_class=IdModel)
 migrate = Migrate()
 
 Session = sessionmaker(bind=db)
