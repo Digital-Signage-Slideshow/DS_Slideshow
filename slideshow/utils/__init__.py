2196403e9fc8ad875a88844856710d88a9512538
Javier Baez <baezdevs@gmail.com>
2022-06-15 22:33:31 -0500

    added file utils and generators

--- slideshow/utils/__init__.py
+++ slideshow/utils/__init__.py
@@ -1,3 +1,6 @@
+import secrets
+import random
+import string
 from slugify import slugify
 
 
@@ -8,3 +11,28 @@ def slugify_text(text, length=50) -> str:
     length: int
     """
     return slugify(text, separator='_', max_length=length, word_boundary=True, save_order=True)
+
+
+def generate_random_string(length=10) -> str:
+    """
+    Generate a random string of length `length`.
+    length: int
+    """
+    i = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+    return slugify_text(i)
+
+
+def get_file_extension(filename: str) -> str:
+    """
+    Get the extension of a file.
+    filename: str
+    """
+    return filename.split('.')[-1]
+
+
+def get_filename(filename: str) -> str:
+    """
+    Get the filename of a file.
+    filename: str
+    """
+    return filename.split('.')[0]
