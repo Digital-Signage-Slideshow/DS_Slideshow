43405584c47508e3b958f8cbab43930d022d2c99
Javier Baez <baezdevs@gmail.com>
2022-07-19 19:29:49 -0500

    added some basic file utils

--- slideshow/utils/__init__.py
+++ slideshow/utils/__init__.py
@@ -1,3 +1,71 @@
+from os import path
+
+
+def get_file_extension(f_path: str) -> str:
+    """
+    Get the file extension.
+
+    Args:
+        f_path: str: path to the file
+
+    Returns:
+        str: file extension
+    """
+    from os import path
+    return path.splitext(f_path)[1][1:]
+
+
+def get_file_name(f_path: str) -> str:
+    """
+    Get the file name.
+
+    Args:
+        f_path: str: path to the file
+
+    Returns:
+        str: file name
+    """
+    return path.basename(f_path)
+
+
+def allowed_file_extensions(f_name: str) -> bool:
+    """
+    Check if the file extension is allowed.
+
+    Args:
+        f_name: str: name of the file
+
+    Returns:
+        bool: True if the file is allowed, False otherwise
+    """
+    from flask import current_app
+    return '.' in f_name and f_name.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
+
+
+def delete_file(f_path: str, f_name: str):
+    """
+    Delete a file if it exists.
+
+    Args:
+        f_path: str: path to the file
+        f_name: str: name of the file
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: if the file doesn't exist
+    """
+    try:
+        from os import path, remove
+        remove(path.join(f_path, f_name))
+    except FileNotFoundError as e:
+        if hasattr(e, 'message'):
+            print(e.message)
+        else:
+            print(e)
+
+
 def create_directory(f_path: str, f_name: str, f_mode: int = 0o777):
     """
     Create a directory if it doesn't exist.
