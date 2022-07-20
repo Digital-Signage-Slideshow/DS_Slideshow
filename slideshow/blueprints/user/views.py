a90141a874803ad07e0a3c85b8330db36c52d1ca
Javier Baez <baezdevs@gmail.com>
2022-07-19 19:30:45 -0500

    updated user routes to use Flask 2

--- slideshow/blueprints/user/views.py
+++ slideshow/blueprints/user/views.py
@@ -1,8 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, jsonify, request
 from flask_login import current_user, login_user, logout_user, login_required
-from werkzeug.security import generate_password_hash
-
-from slideshow.extensions import db
 
 from .forms import RegisterForm, LoginForm, EditProfileForm
 from .models import User
@@ -15,74 +12,72 @@ def custom_500(error: dict):
     response = jsonify({'message': error})
 
 
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.get('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('core.index'))
-
+        return redirect(url_for('core.setup'))
     form = LoginForm()
+    return render_template('user/login.html', form=form)
 
+
+@bp.post('/login')
+def login_post():
+    form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-
         if user is None or not user.check_password(form.password.data):
-            flash('username or password incorrect', 'danger')
-            return redirect(url_for('.login'))
-
+            flash('Invalid username or password')
+            return redirect(url_for('user.login'))
         login_user(user, remember=form.remember.data)
         return redirect(url_for('core.setup'))
+    return render_template('user/login.html', form=form)
 
-    return render_template('user/login.html', title='Login', form=form)
 
-
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.get('/register')
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('core.setup'))
     form = RegisterForm()
-
-    if form.validate_on_submit():
-        u = User(username=form.username.data, email=form.email.data)
-        u.gen_slug(form.username.data)
-        u.hash_password(form.password.data)
-        u.save()
-        return redirect(url_for('user.login'))
-    else:
-        if hasattr(form, 'errors'):
-            for key, value in form.errors.items():
-                flash(value[0], 'warning')
-                break
-
     return render_template('user/register.html', form=form)
 
 
-@bp.route('/logout', methods=['GET', 'POST'])
+@bp.post('/register')
+def register_post():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        User(username=form.username.data, email=form.email.data, password=form.password.data).save()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('user.login'))
+    return render_template('user/register.html', form=form)
+
+
+@bp.get('/logout')
+@login_required
 def logout():
     logout_user()
-    return redirect(url_for('core.index'))
+    return redirect(url_for('user.login'))
 
 
-@bp.route('/<username>', methods=['GET', 'POST'])
+@bp.get('/profile/')
 @login_required
-def profile(username):
-    user = User.query.filter_by(username=username).first_or_404()
+def profile():
     form = EditProfileForm()
+    user = User.query.filter_by(id=current_user.id).first()
+    form.username.data = user.username
+    form.email.data = user.email
+    return render_template('user/profile.html', form=form, user=user)
 
+
+@bp.post('/profile/')
+@login_required
+def profile_post():
+    form = EditProfileForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(id=current_user.id).first()
         current_user.username = form.username.data
         current_user.email = form.email.data
-        current_user.password = generate_password_hash(form.password.data)
-        db.session.commit()
-
-        flash('Your changes have been saved..', 'success')
-        return redirect(url_for('.profile', username=current_user.username))
-
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-
-    return render_template('user/profile.html', title='Profile', user=user, form=form)
-
-
-@bp.route('/edit_profile', methods=['POST'])
-@login_required
-def edit_profile():
-    pass
+        current_user.password = form.password.data
+        user.update()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user.profile'))
+    return render_template('user/profile.html', form=form)
